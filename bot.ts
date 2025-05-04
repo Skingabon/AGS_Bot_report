2b13873e37d7d175f0ea93d52c455cd6d04b4336
skingabon <reclama.katkov@gmail.com>
2025-05-04 22:49:16 +0300

    change button place in bot

--- bot.ts
+++ bot.ts
@@ -24,9 +24,9 @@ bot.api.setMyCommands([
 
 const menuKeyboard = new InlineKeyboard()
 
-  .text('Заполнить исходящие звонки (ручной запуск )', 'generate')
-  .row()
   .text('Создать отчет за последний день (ручной запуск)', 'report-time')
+  .row()
+  .text('Заполнить исходящие звонки (ручной запуск )', 'generate')
   .row()
   .text('Отчет по сделкам за сегодня', 'report-lead-today')
   .row()
